5b940854e24ca8bf0ac1f88133929eb10374a593
aum <aumjavalgikar@gmail.com>
2022-03-29 19:52:44 +0530

    rough work

---
+++ c++ projects/29.3.22/rough/main.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <math.h>
+int main()
+{
+    for(int i = 0; i<=100; i++)
+    {
+        printf("%d\n",i);
+        if(i == 100)
+        {
+            int square_root = sqrt(i);
+            printf("%d\n", square_root);
+        }
+    }
+
+    std::cout << "Chika Chika Slim Shady";
+    return 0;
+}
