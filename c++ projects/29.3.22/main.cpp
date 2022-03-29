540f47df04e8c37636990ac68d4c8e5a894ccc19
aum <aumjavalgikar@gmail.com>
2022-03-29 16:22:55 +0530

    Added #ifdef and #endif

--- c++ projects/29.3.22/main.cpp
+++ c++ projects/29.3.22/main.cpp
@@ -1,10 +1,17 @@
 #include <iostream>
 #define my_name "Slim Shady"
-
+#define JOE
 
 int main()
 {
-    std::cout << "My name is \n" << "My name is \n" << "My name is \n";
+    std::cout << "My name is huh\n" << "My name is what\n" << "My name is who\n";
     std::cout << "Chika Chika " << my_name;
+    #ifdef JOE
+    std::cout << "\nJoe is defined";
+    #endif
+
+    #ifdef BOB
+    std::cout << "BOB is defined";
+    #endif
     return 0;
 }
