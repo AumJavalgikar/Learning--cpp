58f5c5a6c4e5097f5ea62a4c9dd35eb2d1a09e87
aum <aumjavalgikar@gmail.com>
2022-03-29 16:11:02 +0530

    Chika Chika Slim Shady

---
+++ c++ projects/29.3.22/main.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#define my_name "Slim Shady"
+
+
+int main()
+{
+    std::cout << "My name is \n" << "My name is \n" << "My name is \n";
+    std::cout << "Chika Chika " << my_name;
+    return 0;
+}
