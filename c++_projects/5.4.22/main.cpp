2f1d0df337d11d27b26b038e3ce4758b9fe30f7d
aum <aumjavalgikar@gmail.com>
2022-04-05 17:18:49 +0530

    learned to use the debugger in codeblocks, finally

--- c++_projects/5.4.22/main.cpp
+++ c++_projects/5.4.22/main.cpp
@@ -1,5 +1,7 @@
 #include <iostream>
-
+// Go to Debug->Step into to start debugging, when yellow arrow appears, keep stepping into to move to the next
+// part of the program, do not go to next line, use step into. Also, if debugger says "file" or something, make sure your
+// file path has no spaces in it ex- ./git project/project/file.cpp will not work but ./git_project/project/file.cpp will work!
 void Print_int(int a)
 {
     std::cout << a;
