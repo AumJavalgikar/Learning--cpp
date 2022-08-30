40b34c167ff49aaac14eb2c903417b7b8f6338c9
Adi <107060836+voldemort57@users.noreply.github.com>
2022-08-30 17:13:00 +0530

    Update main.cpp

--- c++_projects/March/29.3.22/preprocesser directive #if 0/main.cpp
+++ c++_projects/March/29.3.22/preprocesser directive #if 0/main.cpp
@@ -4,7 +4,7 @@ int main()
 {
     std::cout << "Still\n";
     #if 0                       //made mistake of using #if0 instead of #if 0 before!!
-    std::cout << "not\m";
+    std::cout << "not\n";
     #endif
     std::cout << "D.R.E\n";
     return 0;
